7103450c5a02ba4b0121cfbe56e8f9c74b0ef61e
ase1590 <ase1590@hotmail.com>
2024-07-15 12:52:15 -0500

    add url check

--- pincog/pincog.py
+++ pincog/pincog.py
@@ -54,10 +54,16 @@ class pincog(commands.Cog):
 
     @commands.command()
     @commands.guild_only()
-    async def pinmsg(self, ctx, message_id: int):
+    async def pinmsg(self, ctx, message_id):
         """Pin a message by its ID"""
         if not await self.is_pinner(ctx.author):
             return await ctx.send("You are not authorized for pinning.")
+        if "discord.com/channels/" in message_id:
+            try:
+                parts = message_id.split('/')
+                message_id = parts[-1]
+            except IndexError:
+                return await ctx.send("Invalid link")
         try:
             message = await ctx.channel.fetch_message(message_id)
             await message.pin()
