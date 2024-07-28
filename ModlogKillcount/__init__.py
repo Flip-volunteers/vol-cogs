477258a4015877d67dbe1256a18a3292214f9701
ase1590 <ase1590@hotmail.com>
2024-07-27 21:48:13 -0500

    fix up init imports

--- ModlogKillcount/__init__.py
+++ ModlogKillcount/__init__.py
@@ -1,8 +1,9 @@
 # This init is required for each cog.
 # Import your main class from the cog's folder.
 from .modlogkillcount import ModlogKillcount
+import asyncio
 
 
-def setup(bot):
+async def setup(bot):
     # Add the cog to the bot.
-    bot.add_cog(ModlogKillcount(bot))
+    await bot.add_cog(ModlogKillcount(bot))
